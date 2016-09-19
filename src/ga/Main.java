b9c91d26918661a509c5c7b18be1ac3a2730862d
Nathan Corbyn <me@nathancorbyn.com>
2016-09-19 11:56:15 +0100

    Nested chromosome class

--- src/ga/Main.java
+++ src/ga/Main.java
@@ -197,4 +197,38 @@ public class Main {
 		return fitnessScore;
 	}
 
+	public class Chromosome {
+
+		private int[] genes;
+		private float fitnessScore;
+		
+		/**
+		 * Create a new Chromosome object
+		 */
+		public Chromosome() {
+			genes = new int[Main.EXPRESSION_LENGTH];
+			
+			for(int i = 0; i < genes.length; i++) {
+				genes[i] = Main.rng.nextInt(13);
+			}
+		}
+		
+		public int[] getGenes() {
+			return this.genes;
+		}
+		
+		public void setGenes(int[] genes) {
+			this.genes = genes;
+		}
+	
+		public float getScore() {
+			return fitnessScore;
+		}
+	
+		public void setScore(float score) {
+			this.fitnessScore = score;
+		}
+		
+	}
+
 }
